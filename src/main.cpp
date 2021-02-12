35a29d4fa09fe575625ad2e848e03425f1cd9ad5
Bruno Sá <bruno.vilaca.sa@gmail.com>
2021-02-12 10:40:16 +0000

    fix

--- src/main.cpp
+++ src/main.cpp
@@ -22,10 +22,8 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-#include <producer.h>
-#include <consumer.h>
 
-int main(int argc, char *argv[])
+int MAIN(int argc, char *argv[])
 {
     int a = 0;
     while( a < 50)
